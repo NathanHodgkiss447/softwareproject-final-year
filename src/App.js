c26b3a7b444b6b70b5b21151e39dc5089b31bc3c
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2020-12-07 12:48:33 +0000

    Styling chat room

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./Styles/App.scss";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
+import Chat from "./Components/Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="app_body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app_body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Test</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Chat.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import "../Styles/Chat.scss";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import InfoIcon from "@material-ui/icons/Info";
+
+function Chat() {
+  const { roomId } = useParams();
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <div className="chat__headerLeft">
+          <h4 className="chat__channelName">
+            <strong>#General</strong>
+            <StarBorderIcon />
+          </h4>
+        </div>
+
+        <div className="chat__headerRight">
+          <p>
+            <InfoIcon /> Details
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
