4f5d7bfebb3f2119309bea3c675a1325eda058cb
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2020-12-17 16:43:53 +0000

    Put voice assistant in app.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ import Chat from "./Components/Chat";
 import Resources from "./Components/Resources";
 import SidebarResource from "./Components/SidebarResource";
 import Journal from "./Components/Journal";
-
+import alanBtn from "@alan-ai/alan-sdk-web";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+
+const alanKey =
+  "b676a44263567b4d0d7478389dbdec362e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ testCommand }) => {
+        alert("Test code was exected");
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
