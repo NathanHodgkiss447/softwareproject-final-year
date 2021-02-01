77a8ef9a7a71310a90fb3129265b1a60a30b6b25
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2021-02-01 13:42:50 +0000

    Further styling changes

--- src/Components/SidebarNotes/styles.js
+++ src/Components/SidebarNotes/styles.js
@@ -10,9 +10,9 @@ const styles = (theme) => ({
   newNoteBtn: {
     width: "100%",
     height: "35px",
-    borderBottom: "1px solid black",
+    border: "1px solid gray",
     borderRadius: "0px",
-    backgroundColor: "#29487d",
+    backgroundColor: "#33475b",
     color: "white",
     "&:hover": {
       backgroundColor: "#88a2ce",
